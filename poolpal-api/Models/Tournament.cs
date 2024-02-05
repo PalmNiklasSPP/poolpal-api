633a62ebb8d16fb8d96d98069ed840060975c2ba
Niklas Palm <niklas.palm@spp.se>
2024-02-05 09:49:44 +0100

    Add project files.

---
+++ poolpal-api/Models/Tournament.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace poolpal_api.Models
+{
+    public class Tournament
+    {
+        public int TournamentId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public int ParticipantLimit { get; set; }
+        public bool IsTeamEvent { get; set; }
+        public string TournamentType { get; set; } // Single Elimination, Double Elimination
+
+        // Navigation Properties
+        public ICollection<Match> Matches { get; set; }
+    }
+}
