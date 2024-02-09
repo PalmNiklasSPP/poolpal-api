297487dbee6a51658f539eb2c4c0462b7f4c6079
Timmy Alvelöv <timmy.alvelov@spp.se>
2024-02-09 09:46:33 +0100

    recent matches basic boilerplate

--- poolpal-api/Controllers/StatisticsController.cs
+++ poolpal-api/Controllers/StatisticsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using poolpal_api.Database;
 using poolpal_api.Database.Entities;
+using poolpal_api.Models.PoolTournamentApi.Models;
+using System.Security.Principal;
 
 namespace poolpal_api.Controllers
 {
@@ -15,6 +17,23 @@ namespace poolpal_api.Controllers
             var leaderboardEntries = context.LeaderboardEntries.OrderByDescending(x => x.RankingPoints).ToList();
             return leaderboardEntries;
         }
-       
+        [HttpGet("GetPlayerMatches")]
+        public IEnumerable<Match> GetRecentGames(string user, int? numberOfGames)
+        {
+            var playerWindowsPrinciple = HttpContext.User.Identity as WindowsIdentity;
+            var whatisthis = playerWindowsPrinciple.Name;
+            int playerId = context.Players.Where(p => p.LoginId == user).SingleOrDefault().PlayerId;
+            var gamesIds = context.PlayerMatches.Where(pm => pm.PlayerId == playerId);
+            List<int> matches = new List<int>();
+            foreach(PlayerMatch match in gamesIds)
+            {
+                matches.Add(match.MatchId);
+            }
+            var latestGames = context.Matches.Where(m => matches.Contains(m.MatchId)).OrderByDescending(x => x.MatchDate).ToList();
+
+
+            
+            return latestGames;
+        }
     }
 }
