adc56dbb45ea6fb70c2dbebb634b5c7ab968ed28
SPPTimmy <124141565+SPPTimmy@users.noreply.github.com>
2024-02-13 15:07:04 +0100

    Merge migration into featureb (#6)
    * Added SPPTeams and userSettings
    
    * matchUpdateMigration
    
    ---------
    
    Co-authored-by: Niklas Palm <niklas.palm@spp.se>

---
+++ poolpal-api/Migrations/20240213135135_MatchUpdate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace poolpal_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatchUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PlayerMatches",
+                keyColumns: new[] { "MatchId", "PlayerId" },
+                keyValues: new object[] { 2, 1 },
+                column: "IsWinner",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "PlayerMatches",
+                keyColumns: new[] { "MatchId", "PlayerId" },
+                keyValues: new object[] { 2, 2 },
+                column: "IsWinner",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PlayerMatches",
+                keyColumns: new[] { "MatchId", "PlayerId" },
+                keyValues: new object[] { 2, 1 },
+                column: "IsWinner",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "PlayerMatches",
+                keyColumns: new[] { "MatchId", "PlayerId" },
+                keyValues: new object[] { 2, 2 },
+                column: "IsWinner",
+                value: false);
+        }
+    }
+}
