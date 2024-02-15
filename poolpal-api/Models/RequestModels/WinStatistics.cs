5d4b547e1a88e8ede357eea22d66c8ce19af3b0f
SPPTimmy <124141565+SPPTimmy@users.noreply.github.com>
2024-02-15 14:26:45 +0100

    some more stat endpoints (#8)

---
+++ poolpal-api/Models/RequestModels/WinStatistics.cs
@@ -0,0 +1,9 @@
+﻿namespace poolpal_api.Models.RequestModels
+{
+    public class WinStatistics
+    {
+        public int totalWins { get; set; }
+        public int totalLoses { get; set; }
+        public double winrate { get; set; }
+    }
+}
